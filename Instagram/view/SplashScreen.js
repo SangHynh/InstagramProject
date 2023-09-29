d7cbfe0549647a45a62262c8ccac2085eb0e0c6c
SangHynh <huynhvansang.020102@gmail.com>
2023-09-29 22:01:42 +0700

    done SplashScreen

--- Instagram/view/SplashScreen.js
+++ Instagram/view/SplashScreen.js
@@ -2,13 +2,14 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 export function SplashScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.top}></View>
-      <View style={styles.center}>
-        <Image source={require("../assets/logoIG.png")} style={styles.logo} />
+    <View style = {styles.container}>
+      <View style = {styles.top}></View>
+      <View style = {styles.center}>
+        <Image source = {require("../assets/logoIG.png")} style = {styles.logo} />
       </View>
-      <View style={styles.bottom}>
-        <Text>Meta</Text>
+      <View style = {styles.bottom}>
+        <Text style = {styles.text}>from</Text>
+        <Image source = {require("../assets/icon_meta.png")} style = {styles.metaLogo}/>
       </View>
     </View>
   );
@@ -23,12 +24,28 @@ const styles = StyleSheet.create({
   },
   center: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   bottom: {
     flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   logo: {
     width: 150,
     height: 150,
   },
+  metaLogo: {
+    width: 250,
+    height: 100,
+    resizeMode: 'contain',
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 35,
+    color: 'gray',
+    fontFamily: 'Helvetica',
+    fontWeight: '200'
+  }
 });
